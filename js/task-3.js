a09a034e297171be285c152e5c838c06494fc4fa
Nataliia Rybak <natalibond39@gmail.com>
2025-05-24 22:30:46 +0200

    add HW-03

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,14 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = parseFloat(content);
-  const paddingValue = parseFloat(padding);
-  const borderValue = parseFloat(border);
-  return contentValue + paddingValue * 2 + borderValue * 2;
+function filterArray(numbers, value) {
+  let arr = [];
+  for (const number of numbers) {
+    if (number > value) {
+      arr.push(number);
+    }
+  }
+  return arr;
 }
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
